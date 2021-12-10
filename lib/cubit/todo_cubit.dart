3d6cb4373c8cdd5bc42ca74ff85ec8f8edbea91b
DoungChan <doungchan.sroeun19@kit.edu.kh>
2021-12-10 22:08:02 +0700

    todoscreen

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -1,8 +1,33 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:todo_app/data/model/model.dart';
+import 'package:todo_app/data/repository.dart';
 
 part 'todo_state.dart';
 
 class TodoCubit extends Cubit<TodoState> {
-  TodoCubit() : super(TodoInitial());
+  final Repository repository;
+
+  TodoCubit({required this.repository}) : super(TodoInitial());
+
+  void fetchTodos() {
+    repository.fetchTodos().then((todos) {
+      emit(TodoLoaded(todos: todos));
+    });
+  }
+
+  void changeCompletion(Model todo) {
+    repository.changeCompletion(!todo.isCompleted, todo.id).then((isChanged) {
+      if (isChanged) {
+        todo.isCompleted = !todo.isCompleted;
+        //updateTodoList();
+      }
+    });
+  }
+
+  // void updateTodoList() {
+  //   final currentState = state;
+  //   if (currentState is TodosLoaded)
+  //     emit(TodosLoaded(todos: currentState.todos));
+  // }
 }
--- lib/cubit/todo_state.dart
+++ lib/cubit/todo_state.dart
@@ -4,3 +4,9 @@ part of 'todo_cubit.dart';
 abstract class TodoState {}
 
 class TodoInitial extends TodoState {}
+
+class TodoLoaded extends TodoState {
+  final List<dynamic> todos;
+
+  TodoLoaded({required this.todos});
+}
